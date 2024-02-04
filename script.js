b83d09b10e076b6e372e060f41357dbf1ab32ed0
mmkk911 <myatminkhinkhin09112006@gmail.com>
2024-02-04 23:26:03 +0630

    changed envelope

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const envelope = document.getElementById("envelope");
   const paper = document.getElementById("paper");
+  const line1 = document.getElementById("line1");
+  const line2 = document.getElementById("line2");
   let clickCount = 0;
 
   envelope.addEventListener("click", function () {
     clickCount++;
 
-    if (clickCount < 3) {
+    if (clickCount % 2 !== 0) {
       enlargeEnvelope();
-    } else if (clickCount === 3) {
       openEnvelope();
+    } else {
+      closeEnvelope();
     }
   });
 
   function enlargeEnvelope() {
-    envelope.style.width = `${parseInt(envelope.style.width || 700) + 60}px`;
-    envelope.style.height = `${parseInt(envelope.style.height || 350) + 60}px`;
+    envelope.style.width = `${parseInt(envelope.style.width || 700) + 50}px`;
+    envelope.style.height = `${parseInt(envelope.style.height || 400) + 50}px`;
   }
 
   function openEnvelope() {
     paper.style.opacity = 1;
     paper.innerHTML = "<div class='content'></div>";
     setLargePaper();
+  }
+
+  function closeEnvelope() {
+    envelope.style.width = "700px";
+    envelope.style.height = "400px";
+    paper.style.opacity = 0;
   }
 
   function setLargePaper() {
